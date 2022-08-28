e771be178a6753459fb814047f936d0f27908bdc
Katya <katvas785@gmail.com>
2022-08-28 16:20:01 +0300

    Ex015

---
+++ Ex015/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a==1) Console.WriteLine("Не выходной");
+if(a==2) Console.WriteLine("Не выходной");
+if(a==3) Console.WriteLine("Не выходной");
+if(a==4) Console.WriteLine("Не выходной");
+if(a==5) Console.WriteLine("Не выходной");
+if(a==6) Console.WriteLine("Выходной");
+if(a==7) Console.WriteLine("Выходной");
