ae8d1b96d8e0eac915178c3b8920e81b8b9abea0
Katya <katvas785@gmail.com>
2022-08-28 16:26:55 +0300

    Ex013

---
+++ Ex013/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = 645;
+if (100 < n)
+if (n < 1000)
+{
+    Console.WriteLine(n % 10);
+}
+if (0 < n)
+if (n < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+if (1000 < n)
+if (n < 10000)
+{
+    Console.WriteLine ((n % 100) / 10);
+}
